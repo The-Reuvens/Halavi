0cf61e30afacd7417c63b021447963aabc7db01c
Doinkk <helwing00@gmail.com>
2024-08-18 16:30:01 +0300

    player is falling down + starting y velocity and max Y velocity

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,14 @@ public class Player : MonoBehaviour
 {
     private bool isDragging = false;
     public float FollowSpeed = 13;
+    private float startingVelocityY = -100;
+    private float maxYVelocity = -120;
+    [SerializeField] private Rigidbody playerContainerRB;
 
+    private void Start()
+    {        
+        playerContainerRB.velocity = new Vector3(0, startingVelocityY, 0);
+    }
     void FixedUpdate()
     {
         if (isDragging && Mouse.current.leftButton.isPressed)
@@ -13,8 +20,14 @@ public class Player : MonoBehaviour
             Vector3 targetPosition = Input.mousePosition;
             targetPosition.z = -Camera.main.transform.localPosition.z;
             targetPosition = Camera.main.ScreenToWorldPoint(targetPosition);
-
             transform.position = Vector3.Lerp(transform.position, targetPosition, FollowSpeed * Time.deltaTime);
+        }
+
+        /*if current y velocity is above max velocity,cancel out gravity by adding an equal force the opposite direction*/
+        print(playerContainerRB.velocity);
+        if (playerContainerRB.velocity.y < maxYVelocity)
+        {
+            playerContainerRB.AddForce(-Physics.gravity);
         }
     }
 
