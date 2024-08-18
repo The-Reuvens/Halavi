c9e7b765ea50cf4d384135c1ec2ed39e32d074b8
liogiladi <liogiladi@gmail.com>
2024-08-18 13:11:50 +0300

    made mouse position to player work

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,18 +9,17 @@ public class Player : MonoBehaviour
 
     void Start()
     {
-        
+
     }
 
     void FixedUpdate()
     {
         if (Mouse.current.leftButton.isPressed == true)
         {
-            Vector3 targetPos;
-            Vector3 mousePos = Mouse.current.position.value;
-            targetPos = Camera.main.ScreenToWorldPoint(new Vector3(Mouse.current.position.value.x, Mouse.current.position.value.y, -Camera.main.transform.position.z));
-            targetPos.z = transform.position.z;
-            transform.position = targetPos;
+            Vector3 mousePos = Input.mousePosition;
+            mousePos.z = -Camera.main.transform.localPosition.z;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+            transform.position = mousePos;
         }
     }
 }
