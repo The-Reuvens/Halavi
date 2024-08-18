0b8445c58415d16fad001296a804333be40b2d54
liogiladi <liogiladi@gmail.com>
2024-08-18 15:49:31 +0300

    add player animator + animations

---
+++ Assets/Scripts/WeightManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class WeightManager : MonoBehaviour
+{
+    private float weight;
+    public float Weight
+    {
+        get { return weight; }
+        set
+        {
+            weight = value;
+        }
+    }
+
+
+}
